fce95c8c8605ac9ad70e14a8ff46101b68add9d1
Aleksey Belyanin <alekseybelyanin@Alekseys-MacBook-Air.local>
2017-06-10 16:46:54 +0300

    Initial Commit

---
+++ множества(деревья)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  множества(деревья)
+//
+//  Created by Aleksey Belyanin on 10/06/2017.
+//  Copyright © 2017 Aleksey Belyanin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
